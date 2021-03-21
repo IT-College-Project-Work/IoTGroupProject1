89641f93456dd5c380270095e079e99a629c89c6
StevenK418 <stevenkelly418@gmail.com>
2021-03-21 11:38:25 +0000

    Added comment to the BeamThreshold variable.

--- PostureCorrection/PostureCorrection.ino
+++ PostureCorrection/PostureCorrection.ino
@@ -12,7 +12,7 @@ char devid[] = "v17175AEC3026700";
 char serverName[] = "api.pushingbox.com";
 boolean DEBUG = true;
 
-const int BEAMTHRESHOLD = 100;
+const int BEAMTHRESHOLD = 100;    //Should be tweaked to suit lighting conditions of running environment. 
 const int LED = 13;
 const int SENSOR = A3;
 const int LOUDSPEAKER;
